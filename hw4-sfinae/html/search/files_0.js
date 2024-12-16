9a768f6ab11ae95030696859d2062770e643ae44
dmitriirylko <drv01101998@gmail.com>
2024-12-16 12:03:29 +0300

    html folder deleted

--- hw4-sfinae/html/search/files_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['printip_2eh_2',['printip.h',['../printip_8h.html',1,'']]]
-];
--- hw4-sfinae/html/search/functions_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['print_5fip_3',['print_ip',['../printip_8h.html#a467885b25bbe6dffe459e7caad59bcdd',1,'print_ip(T t):&#160;printip.h'],['../printip_8h.html#ae6f4cdf35193f957ccc1fea7183998d3',1,'print_ip(const T &amp;t):&#160;printip.h'],['../printip_8h.html#ab9e6ee26342c593c72144c3d3cb28117',1,'print_ip(const Container&lt; T, Allocator &gt; &amp;t):&#160;printip.h'],['../printip_8h.html#aa9ed071d1da0451bdd3446c119822b02',1,'print_ip(std::tuple&lt; Ts... &gt; t):&#160;printip.h']]]
-];
